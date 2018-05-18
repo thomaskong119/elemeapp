6b880445d27e527fb0675478e8b0880a623cbf6f
thomaskong <thomaskong119@hotmail.com>
2018-05-19 00:22:00 +0800

    1

--- getallcomment.py
+++ getallcomment.py
@@ -5,6 +5,7 @@ from datetime import datetime
 from datetime import timedelta
 import time
 import schedule
+import math
 
 def job():
 
@@ -18,35 +19,70 @@ def job():
 
     params = json.dumps(data).encode('utf8')
     req = urllib.request.Request(url, data=params, headers=header)
-    res = urllib.request.urlopen(req)
+    try:
+        res = urllib.request.urlopen(req)
+        d1 = json.load(res)
+        d1 = d1['result']['result']
 
-    d1 = json.load(res)
-    d1 = d1['result']['result']
+        count = 0
+        scoresum = 0
+        remain = 0
+        
+        for index in range(len(d1)):
+            t1 = datetime.strptime(d1[index]['createTime'], '%Y-%m-%d %H:%M:%S')
+            d = datetime.now() - timedelta(days=30)
+            if t1 > d:
+                file_object.write("\n" + str(d1[index]['orderNO']) +" "+ str(d1[index]['compositionalScore'])+" "+ str(d1[index]['createTime']))
+                count += 1
+                scoresum += int(d1[index]['compositionalScore'])
+            else:
+                break
 
-    # for index in range(len(d1)):
-    #     file_object.write("\n" + str(d1[index]['orderNO']) +" "+ str(d1[index]['compositionalScore']))
-    count = 0
-    scoresum = 0
-    
-    for index in range(len(d1)):
-        t1 = datetime.strptime(d1[index]['createTime'], '%Y-%m-%d %H:%M:%S')
-        d = datetime.now() - timedelta(days=30)
-        if t1 > d:
-            file_object.write("\n" + str(d1[index]['orderNO']) +" "+ str(d1[index]['compositionalScore']))
-            count += 1
-            scoresum += int(d1[index]['compositionalScore'])
-        else:
-            break
+        scorenow = (round_up(scoresum / count*10000))/10000
+        content = "目前评分：" + str(scorenow) + "\n"
+        for score in range(math.ceil(scoresum / count*10), 51, 1):
+            while round_up(scoresum / count)*10 < score:
+                scoresum += 5.0
+                count += 1
+                remain += 1
+            content += "距离" + str(score/10) + "分还差" + str(remain) + "条好评" + "\n"
 
-    scoreaverage = scoresum / count
-    print (scoreaverage)
-    file_object.write(str(datetime.now())+"\n===========================================")
+        print (content)
+    except urllib.error.HTTPError:
+        print ("HTTPError, will try again")
+        file_object.write("HTTPError")
+        job()
+
+    file_object.write("\n" + str(datetime.now())+"\n===========================================")
     file_object.close()
+    print (str(datetime.now()))
+
+    # mobilelist = "18600536524"
+    # dingurl = 'https://oapi.dingtalk.com/robot/send?access_token=549961be9efed1a4a1c56318e3480834521ad64920f9d7e29263dbb4bb0d30a8'
+    # dingdata = {
+    #     "msgtype": "text",
+    #     "text": {
+    #         "content": content
+    #     },
+    #     "at": {
+    #         "atMobiles": [
+    #             mobilelist
+    #         ],
+    #         "isAtAll": False
+    #     }
+    # }
+    # json_str = json.dumps(dingdata).encode('utf8')
+    # dingreq = urllib.request.Request(dingurl, data=json_str, headers= header)
+    # dingres = urllib.request.urlopen(dingreq)
+    # print (dingres.read())
+
+def round_up(value):     
+      return round(value * 10) / 10.0
 
 job()
-# schedule.every(0.2).minutes.do(job)
-schedule.every(1).hour.do(job)
+for runtime in range(8,24):
+    schedule.every().day.at(str(runtime)+":59").do(job)
 
 while True:
     schedule.run_pending()
-    time.sleep(0.01)
+    time.sleep(1)
--- runjobs.py
+++ runjobs.py
@@ -4,6 +4,7 @@ from pprint import pprint
 from datetime import datetime
 import time
 import schedule
+import math
 
 ordertemp = [0]*20
 orderlast = [0]*20
@@ -76,24 +77,66 @@ def jobgetallcomment():
         data = json.load(f)
     
     url = 'https://app-api.shop.ele.me/buttonwood/invoke/?method=GadgetzanAPIService.getAppraisalListByServiceNO'
-    header = {'Content-Type': 'application/json'}
 
-    file_object = open('commentdetail.txt', 'a') 
+    file_object = open('commentdetail.txt', 'w') 
 
     params = json.dumps(data).encode('utf8')
     req = urllib.request.Request(url, data=params, headers=header)
-    res = urllib.request.urlopen(req)
+    try:
+        res = urllib.request.urlopen(req)
+        d1 = json.load(res)
+        d1 = d1['result']['result']
 
-    d1 = json.load(res)
-    d1 = d1['result']['result']
+        count = 0
+        scoresum = 0
+        remain = 0
+        
+        for index in range(len(d1)):
+            t1 = datetime.strptime(d1[index]['createTime'], '%Y-%m-%d %H:%M:%S')
+            d = datetime.now() - timedelta(days=30)
+            if t1 > d:
+                file_object.write("\n" + str(d1[index]['orderNO']) +" "+ str(d1[index]['compositionalScore'])+" "+ str(d1[index]['createTime']))
+                count += 1
+                scoresum += int(d1[index]['compositionalScore'])
+            else:
+                break
 
-    print (d1[0])
+        scorenow = (round_up(scoresum / count*10000))/10000
+        content = "目前评分：" + str(scorenow) + "\n"
+        for score in range(math.ceil(scoresum / count*10), 51, 1):
+            while round_up(scoresum / count)*10 < score:
+                scoresum += 5.0
+                count += 1
+                remain += 1
+            content += "距离" + str(score/10) + "分还差" + str(remain) + "条好评" + "\n"
 
-    for index in range(len(d1)):
-        file_object.write("\n" + str(d1[index]['orderNO']) +" "+ str(d1[index]['compositionalScore']))
+        print (content)
+    except urllib.error.HTTPError:
+        print ("HTTPError, will try again")
+        file_object.write("HTTPError")
+        jobgetallcomment()
 
-    file_object.write(str(datetime.now())+"\n===========================================")
+    file_object.write("\n" + str(datetime.now())+"\n===========================================")
     file_object.close()
+
+    mobilelist = "18600536524"
+    dingdata = {
+        "msgtype": "text",
+        "text": {
+            "content": str(d1[0]['createTime'])+"有新的差评，"+str(d1[0]['compositionalScore'])+"分来自于"+str(d1[0]['valuator'])+"用户说"+str(d1[0]['content'])
+        },
+        "at": {
+            "atMobiles": [
+                mobilelist
+            ],
+            "isAtAll": False
+        }
+    }
+    json_str = json.dumps(dingdata).encode('utf8')
+    dingreq = urllib.request.Request(dingurl, data=json_str, headers= header)
+    dingres = urllib.request.urlopen(dingreq)
+    print (dingres.read() + "\n--getallcomment")
+
 
 def jobsendPostDing():
 
@@ -103,8 +146,7 @@ def jobsendPostDing():
     file_object = open('ordercount.txt', 'a') 
 
     url = 'https://app-api.shop.ele.me/buttonwood/invoke/?method=ClassifyService.getServicesByClassifyCode'
-    header = {'Content-Type': 'application/json'}
-
+    
     params = json.dumps(data).encode('utf8')
     req = urllib.request.Request(url, data=params, headers=header)
     res = urllib.request.urlopen(req)
@@ -123,7 +165,6 @@ def jobsendPostDing():
     file_object.write(str(datetime.now())+"\n===========================================\n")
     file_object.close()
 
-    dingurl = 'https://oapi.dingtalk.com/robot/send?access_token=549961be9efed1a4a1c56318e3480834521ad64920f9d7e29263dbb4bb0d30a8'
     mobilelist = "18513249383"
     dingdata = {
         "msgtype": "text",
@@ -157,12 +198,16 @@ def jobsendPostDing():
         print (dingres.read())
         pass
 
+def round_up(value):     
+      return round(value * 10) / 10.0
+
 jobsendPostDing()
 jobgetcomment()
 jobgetallcomment()
-schedule.every(1).hour.do(jobsendPostDing)
-schedule.every().day.do(jobgetallcomment)
+schedule.every().day.at("10:00").do(jobgetallcomment)
 schedule.every(0.2).minutes.do(jobgetcomment)
+for runtime in range(8,24):
+    schedule.every().day.at(str(runtime)+":59").do(jobsendPostDing)
 
 while True:
     schedule.run_pending()
